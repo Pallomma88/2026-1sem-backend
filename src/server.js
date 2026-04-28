e2beb6aadc3fab7364ebb15a5be3bd2977a95dfa
Pallomma88 <pallomma88@gmail.com>
2026-04-27 21:00:46 -0300

    exercico 27/04/26

--- src/server.js
+++ src/server.js
@@ -6,7 +6,21 @@ function callbackDaRaiz( request, response ) {
     response.send("VAI CORINTHIANS!!!! 🦅")
 }
 
+function callbackDaRaiz(request, response) {
+    const listaUsuarios = [
+        {
+            id: 1,
+            name:"Pallomma"
+        }
+    ]
+        
+     response.json(listaUsuarios)
+    }
+
+
 app.get("/", callbackDaRaiz)
+
+app.get("/users", callbackDaRaiz)
 
 app.listen(3000, () => {
     console.log("API rodando em http://localhost:3000")
